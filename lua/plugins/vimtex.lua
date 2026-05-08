e1a5fe0f22415f003e4e2381d5407498ac92680e
Luc Tanner <lutanner@student.ethz.ch>
2026-05-08 19:39:42 +0200

    Vimtex: disabled overfull/underfull compiler warinings

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -20,7 +20,14 @@ return {
     end
     vim.g.vimtex_compiler_method = 'latexmk'
     vim.g.vimtex_view_automatic = 1
+
     vim.g.vimtex_imaps_enabled = 0 --disables vimtex mappings in insert mode
+
+    -- Disables Overfull, Underfull compiler warnings
+    vim.g.vimtex_quickfix_ignore_filters = {
+      'Overfull',
+      'Underfull',
+    }
   end,
   keys = {
     { '<leader>lc', '<cmd>VimtexCompile<cr>', desc = 'LaTeX: Compile' },
