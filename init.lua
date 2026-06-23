772636e06486fce1aa6729089f3ce93aa380b242
Luc Tanner <lutanner@student.ethz.ch>
2026-06-23 23:02:38 +0200

    c/cpp support

--- init.lua
+++ init.lua
@@ -367,7 +367,7 @@ require('lazy').setup({
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         pyright = {},
         texlab = {
@@ -461,7 +461,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -474,6 +474,8 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'black' },
+        cpp = { 'clang-format' },
+        c = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -620,6 +622,8 @@ require('lazy').setup({
         ensure_installed = {
           'bash',
           'c',
+          'cpp',
+          'cmake',
           'diff',
           'html',
           'lua',
